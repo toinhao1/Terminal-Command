56ce5b15f8873f7f535c9f58547617e05e75b978
toinhao1 <toinhao89@gmail.com>
2018-11-18 18:54:11 -0600

    Checkpoint 3 Modules

--- terminal-commands.js
+++ terminal-commands.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
+
 module.exports.ls = () => {
   fs.readdir('./', (err, files) => {
     const filesToString = files.reduce((acc, file) => {
       return '${acc} ${file} ';
     }, '');
     console.log(filesToString);
+  });
+};
+module.exports.touch = () => {
+  fs.writeFile('./', data, (err) => {
+    if (err) throw err;
+    console.log('File has been saved!')
+  });
+};
+module.exports.mkdir = () => {
+  fs.mkdir('./', (err) => {
+    if (err) throw err;
   });
 };
